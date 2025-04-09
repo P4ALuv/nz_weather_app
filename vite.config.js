538b3394cd68df807d5fddfa410b3ca1ee625897
P4ALuv <161944168+P4ALuv@users.noreply.github.com>
2025-04-10 03:52:40 +1200

    already deployed

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  base: '/nz_weather_app/', // ← 这里是你仓库名
   plugins: [react()],
 })
